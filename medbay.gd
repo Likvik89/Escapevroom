c0954b5221dd2e2409cb28888306bc17e73d837a
Likvik <erik@elfisk.dk>
2024-10-10 12:49:27 +0200

    more content

--- medbay.gd
+++ medbay.gd
@@ -2,25 +2,46 @@ extends Node2D
 
 var medbay = true
 var hallway = false
+var central_hall = false
+var engineroom = false
+var controlroom = false
+var escapepod = false
+var dead = false
 
-
-
-
-
+#Opdaterer rummet efter hvor spilleren er
 func _process(delta):
+	#Viser de ting som er i medbay
 	if medbay:
 		%Medbay_doorway.visible = true
 		%Medbay.visible = true
 	else:
 		%Medbay_doorway.visible = false
-		%Medbay.visible = true
-		pass
+		%Medbay.visible = false
+	#Viser de ting som er i hallway'en
 	if hallway:
 		%Hallway.visible = true
 		%Hallway_goback.visible = true
+		%Hallway_goforward.visible = true
 	else:
 		%Hallway.visible = false
 		%Hallway_goback.visible = false
+		%Hallway_goforward.visible = false
+	#Viser de ting som er i den centrale hall
+	if central_hall:
+		%SpaceshipSal.visible = true
+		%Sal_goback.visible = true
+		%Controlroom.visible = true
+		%Engineroom.visible = true
+	else:
+		%SpaceshipSal.visible = false
+		%Sal_goback.visible = false
+		%Controlroom.visible = false
+		%Engineroom.visible = false
+	#viser skibets motor
+	if engineroom:
+		%Engine.visible = true
+	else:
+		%Engine.visible = false
 
 func _on_hallway_goback_pressed() -> void:
 	hallway = false
@@ -29,3 +50,16 @@ func _on_hallway_goback_pressed() -> void:
 func _on_medbay_doorway_pressed() -> void:
 	medbay = false
 	hallway = true
+
+func _on_hallway_goforward_pressed() -> void:
+	central_hall = true
+	hallway = false
+
+func _on_sal_goback_pressed() -> void:
+	central_hall = false
+	hallway = true
+
+
+func _on_engineroom_pressed() -> void:
+	central_hall = false
+	engineroom = true
